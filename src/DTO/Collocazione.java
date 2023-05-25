cd7976298cbf2a0f52a3c9f2843c83858f064bef
Raul Dalli <107949769+rolerzz@users.noreply.github.com>
2023-05-25 23:55:04 +0200

    Add files via upload

---
+++ src/DTO/Collocazione.java
@@ -0,0 +1,17 @@
+package DTO;
+
+public class Collocazione {
+
+	private String Sede;
+	private String Regione;
+
+
+	public String getSede() { return Sede; }
+
+	public void setSede(String sede) { Sede = sede; }
+
+
+	public String getRegione() { return Regione; }
+
+	public void setRegione(String regione) { Regione = regione; }
+}
